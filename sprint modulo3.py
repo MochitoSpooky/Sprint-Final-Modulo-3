09861476b22da79b16f1a2b7f6f0c2f30fdbbea4
MochitoSpooky <119640815+MochitoSpooky@users.noreply.github.com>
2023-05-01 21:09:35 -0400

    Update sprint modulo3.py
    creacion de contraseña al azar y numero telefonico

--- sprint modulo3.py
+++ sprint modulo3.py
@@ -4,3 +4,12 @@ usuarios = ['usuario1', 'usuario2', 'usuario3', 'usuario4', 'usuario5', 'usuario
 
 cuentas = {}
 
+def crear_cuenta(usuario):
+    #crear contraseña con requerimientos solicitados, mayusculas, minusculas y numeros
+    password = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', k=8))
+    print(f"La contraseña de {usuario} es: {password}")
+    #solicitar numero telefonico con al menos 8 digitos
+    while True:
+        numero = input(f"Ingrese el numero telefonico de {usuario}: ")
+        if numero.isdigit() and len(numero) == 8:
+            break
