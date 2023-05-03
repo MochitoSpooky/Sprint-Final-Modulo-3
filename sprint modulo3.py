89cf62c7d64121afea5a0f152778515c524faab8
MochitoSpooky <119640815+MochitoSpooky@users.noreply.github.com>
2023-05-02 20:53:40 -0400

    Update sprint modulo3.py
    edicion de comentario

--- sprint modulo3.py
+++ sprint modulo3.py
@@ -15,7 +15,7 @@ def crear_cuenta(usuario):
     #crear contraseña con requerimientos solicitados, mayusculas, minusculas y numeros
     password = ''.join(random.choices(alfabeto, k=8))
     print(f"La contraseña de {usuario} es: {password}")
-    #solicitar numero telefonico con al menos 8 digitos
+    #solicitar numero telefonico de 8 digitos
     while True:
         numero = input(f"Ingrese el numero telefonico de {usuario}: ")
         if numero.isdigit() and len(numero) == 8:
